10d58c5a64b8c2598166dba8537e5f7bec9ef712
yashkumar2603 <yash.2603kumar@gmail.com>
2023-12-15 14:34:59 +0530

    couldnt solve, revisit

---
+++ A_Reverse_a_Substring.cpp
@@ -0,0 +1,33 @@
+
+// COULDNT SOLVE< MARKED FOR REVISIT.
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+ll lcm(ll a, ll b) { return a / __gcd(a, b) * b; }
+
+// Debug, remove before submit.
+/*void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { cerr<<' '<< H; dbg_out(T...); }
+#define dbg(...) cerr << '(' << #__VA_ARGS__<<'):', dbg_out(__VA_ARGS__)*/
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    char s[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+    }
+
+    int l, r;
+
+    return 0;
+}
